9b94fb5bc48cd2a21271d7baeb0ea91a3f3f9d6e
Jordan Yaqoob <jordanyaqoob2@gmail.com>
2017-11-13 00:33:33 -0500

    can create chain cannot generate text

--- 11/mc.py
+++ 11/mc.py
@@ -5,6 +5,22 @@ def remove_nonalpha(w):
             result = result + l
     return result
 
+def make_chain(f):
+    f = open(f).read()
+    l = f.split()
+    l = [remove_nonalpha(x) for x in l]
+    l = [x.lower() for x in l]
+    d = {}
+    for i in range(2,len(l)):
+        a = l[i-2]
+        b = l[i-1]
+        d.setdefault((a,b),[])
+        d[(a,b)].append(l[i])
+    return d
+
+def generate_text(d, start_word, length=50):
+    wordlist = []
+    
 
 
-print word_dict('hamlet.txt')
+print make_chain('hamlet.txt')
