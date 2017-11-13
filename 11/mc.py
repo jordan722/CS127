5a929f6e260e941756c9a11c07dfa5c6bcb6a06b
Jordan Yaqoob <jordanyaqoob2@gmail.com>
2017-11-13 00:48:33 -0500

    homework fixed

--- 11/mc.py
+++ 11/mc.py
@@ -1,3 +1,4 @@
+import random
 def remove_nonalpha(w):
     result=''
     for l in w:
@@ -20,7 +21,18 @@ def make_chain(f):
 
 def generate_text(d, start_word, length=50):
     wordlist = []
-    
+    l = dict((k,v) for k,v in d.items() if k[1] == start_word)
+    curr = start_word
+    n = random.choice(random.choice(l.values()))
+    print n
+    for i in range(length):
+        if (curr,n) not in d:
+            break
+        wordlist.append(n)
+        key = (curr,n)
+        curr = n
+        n = random.choice(d[key])
+    return " ".join(wordlist)
 
 
-print make_chain('hamlet.txt')
+print generate_text(make_chain('hamlet.txt'), 'to')
